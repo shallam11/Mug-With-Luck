3150b78e67fff0cce051a2f7b7f9df84d1e8b171
matthijs12310 <bobstoffer1596@gmail.com>
2023-02-11 14:53:54 +0000

    :(

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ console.log("Hi")
 particlesJS.load('particles-js', 'particles.json', function() {
     console.log('callback - particles.js config loaded');
   });
+
+
+$(".btn").click(function(){
+    $(".text").fadeOut().delay(400)
+    $(".designs").fadeIn()
+});
